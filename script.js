2fcae80efcf58c25ccf6917f360441cfea211d9f
DantasUX09 <dantasguierkljmiejuhn>
2022-11-10 13:30:07 -0300

    index principal finalizado

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const sexoM = document.querySelector(".menu-item-homen button");
 function ativar(event){
   const menu = event.currentTarget;
   
+  const mulher = document.querySelector("#mulher");
   const homen = document.querySelector("#homen1");
   homen.classList.toggle("ativo")
+  mulher.classList.remove("ativo")
   console.log(homen)
 
 }
@@ -18,8 +20,10 @@ const sexoF = document.querySelector(" .menu-item-mulher button");
 function ativarF(event){
 const menu =event.currentTarget;
 
+const homen = document.querySelector("#homen1");
 const mulher = document.querySelector("#mulher");
 mulher.classList.toggle("ativo")
+homen.classList.remove("ativo")
 }
 
 sexoF.addEventListener("click", ativarF)
